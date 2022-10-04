95e70438e21fca4d36f40afd0d154cf1dac3ff2e
David Demerchyan <davidemer11@yandex.ru>
2022-10-04 17:58:12 +0300

    created table

--- main/main.py
+++ main/main.py
@@ -1,14 +1,36 @@
 import tkinter as tk
 
 
-window = tk.Tk()
+class Minesweeper:
+
+    window = tk.Tk()
+    ROW = 10
+    COLUMNS = 7
+
+    def __init__(self):
+        self.buttons = []
+        for i in range(self.ROW):
+            interim = []
+            for j in range(self.COLUMNS):
+                btn = tk.Button(self.window, width=3, font='Calibri 15 bold')
+                interim.append(btn)
+            self.buttons.append(interim)
+
+    def create_widgets(self):
+        for i in range(self.ROW):
+            for j in range(self.COLUMNS):
+                btn = self.buttons[i][j]
+                btn.grid(row=i, column=j)
+
+    def start(self):
+        self.create_widgets()
+        self.print_buttons()
+        self.window.mainloop()
+
+    def print_buttons(self):
+        for row_btn in self.buttons:
+            print(row_btn)
 
 
-
-
-
-
-
-
-
-tk.mainloop()
+game = Minesweeper()
+game.start()
